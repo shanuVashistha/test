7f93f8333657a931f2f112ac8415b50051cf2e0f
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-05-09 19:30:55 +0530

    Update Next COnfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' }
+    };
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
